38b4ffa6235d84546995f20d79cc0afec3a6bdf7
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2019-11-04 10:03:27 -0500

    Initial Committ

--- dicrisif_Assignment09/Form1.Designer.cs
+++ dicrisif_Assignment09/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 512);
+            this.ClientSize = new System.Drawing.Size(974, 583);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
